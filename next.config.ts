bb210cacc14f9bff60cafa1b925ff984b4ed11b7
interview-nx <ischenko.vadyus@gmail.com>
2025-01-30 16:19:29 +0200

    testing ci

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,11 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    basePath: isProd ? "/auth-clario" : "",
-    assetPrefix: isProd ? "/auth-clario/" : "",
-    trailingSlash: true,
   },
+  basePath: isProd ? "/auth-clario" : "",
+  assetPrefix: isProd ? "/auth-clario/" : "",
+  trailingSlash: true,
+  output: "export",
 };
 
 export default nextConfig;
